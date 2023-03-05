b03c3e0b837751e8caf62cba35b3057bb563f9fe
SerhiiDemydov <serjdemidov95@gmail.com>
2023-03-05 14:56:43 +0200

    Added logger and message about successfully answer to user

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-
+import logging
 import parameters
 import telebot
 import datetime
@@ -7,22 +7,18 @@ import json
 import os
 from threading import Thread
 
-# generate_service = {}
 BOT_TOKEN = parameters.bot_token
 bot = telebot.TeleBot(BOT_TOKEN)
 
+logging.basicConfig(level='INFO')
+logger = logging.getLogger()
 
-# keyboard_phone = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
-# button_phone = telebot.types.KeyboardButton(text="Send phone", request_contact=True)
-# keyboard_phone.add(button_phone)  # Add this button
-#
-# keyboard_finish = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
-# button_finish = telebot.types.KeyboardButton(text="Finish")
-# keyboard_finish.add(button_finish)
-#
-# keyboard_service = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
-# button_service = telebot.types.InlineKeyboardButton(text="Create service request")
-# keyboard_service.add(button_service)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler = logging.FileHandler('logfile.log')
+handler.setLevel(level=logging.DEBUG)
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
 
 
 @bot.message_handler(commands=['start', 'hello'])
@@ -30,40 +26,7 @@ def send_welcome(message):
 	bot.reply_to(message,
 	             f"Вітаю, {message.from_user.first_name}. Я бот сервісної підтримки Sasha Assaul Design. "
 	             f"Опишіть будь ласка ваш запит чи проблему і з вами зв'яжуться наші фахівці.")
-	work_time = True
-
-
-#
-# @bot.message_handler(content_types=['contact'])
-# def contact(message):
-# 	if message.contact is not None:
-# 		print(message.contact)
-# 		with open(f"{message.from_user.username}_{datetime.datetime.fromtimestamp(message.date).date()}.txt", "w+") as file:
-# 			file.write(f'Request from {message.from_user.first_name} {message.from_user.last_name} - Phone {message.contact.phone_number}\n')
-# 		bot.reply_to(message, f"{message.from_user.first_name}, please descript your problem", reply_markup=keyboard_finish)
-# 		generate_service.update({message.from_user.username: True})
-# 	print(generate_service)
-
-
-# @bot.message_handler(func=lambda message: message.text == "Create service request")
-# def activate_service_request(message):
-# 	bot.send_message(message.chat.id, f"{message.from_user.first_name}, please give your number", reply_markup=keyboard_phone)
-
-
-# @bot.message_handler(content_types=["text"])
-# def save_message(message):
-# 	try:
-# 		if generate_service[message.from_user.username]:
-# 			if message.text == "Finish":
-# 				generate_service.pop(message.from_user.username)
-# 				bot.reply_to(message, f"Thank you, {message.from_user.first_name}")
-# 			else:
-# 				with open(f"{message.from_user.username}_{datetime.datetime.fromtimestamp(message.date).date()}.txt", "a+") as file:
-# 					file.write(f'{datetime.datetime.fromtimestamp(message.date)} - {message.text}\n')
-# 		print(generate_service)
-# 	except KeyError:
-# 		bot.reply_to(message, f"{message.from_user.first_name}, if you have some problem. Please, click on 'Create service request'", reply_markup=keyboard_service)
-#
+	logger.info('Send hello message')
 
 @bot.message_handler(
 	content_types=["text", "audio", "document", "photo", "sticker", "video", "video_note", "voice", "location",
@@ -73,28 +36,41 @@ def send_welcome(message):
 def forward_message(message):
 	# Надсилання повідомлення клієнту
 	if message.chat.id == parameters.group_id_main:
-		
-		# print(message.reply_to_message)
-		if message.reply_to_message.forward_sender_name:
-			name = message.reply_to_message.forward_sender_name
-			print(name)
-			print(f'Send to {name.split(" ")[0]}')
-			bot.send_message(users[name.split(" ")[0]]["chat_id"], message.text)
-			users[name.split(" ")[0]].update({"send_answer": True})
-		else:
-			print(f'Send to {message.reply_to_message.forward_from.first_name}')
-			bot.send_message(message.reply_to_message.forward_from.id, message.text)
-			users[message.reply_to_message.forward_from.first_name].update({"send_answer": True})
+		# try:
+			if message.reply_to_message.forward_from:
+				# print(message.reply_to_message)
+				if message.reply_to_message.forward_sender_name:
+					name = message.reply_to_message.forward_sender_name
+					bot.send_message(users[name.split(" ")[0]]["chat_id"], message.text)
+					users[name.split(" ")[0]].update({"send_answer": True})
+					bot.reply_to(message,
+					             f"Повідомлення надіслано користувачу {name.split(' ')[0]} (@{users[name.split(' ')[0]]['username']})")
+					logger.info(f"Send answer to {name.split(' ')[0]} (@{users[name.split(' ')[0]]['username']}_")
+				else:
+					print(f'Send to {message.reply_to_message.forward_from.first_name}')
+					bot.send_message(message.reply_to_message.forward_from.id, message.text)
+					users[message.reply_to_message.forward_from.first_name].update({"send_answer": True})
+					bot.reply_to(message,
+					             f"Повідомлення надіслано користувачу {message.reply_to_message.forward_from.first_name} "
+					             f"(@{users[message.reply_to_message.forward_from.first_name]['username']})")
+					logger.info(f"Send answer to {message.reply_to_message.forward_from.first_name} "
+					             f"(@{users[message.reply_to_message.forward_from.first_name]['username']})")
+		# except AttributeError:
+		# 	bot.reply_to(message,
+		# 	             f"Повідомлення не відправлене")
+		# 	logger.info(f"Don't sent answer")
+		# raise AttributeError
 	# Перенаправлення повідомлення до групи підтримки
 	else:
 		bot.forward_message(parameters.group_id_main, message.chat.id, message.message_id)
-		print(f"From {message.chat.id}")
-		# print(message)
+		print(f"From {message.from_user.first_name} - {message.chat.id}")
+		logger.info(f"Received message from {message.from_user.first_name} - {message.chat.id}")
 		users.update(
 			{
 				f"{message.from_user.first_name}":
 					{
 						"chat_id": message.chat.id,
+						"username": message.from_user.username,
 						"time_last_message": str(datetime.datetime.fromtimestamp(message.date)),
 						"time_answer_message": str(
 							datetime.datetime.fromtimestamp(message.date) + datetime.timedelta(minutes=2)),
@@ -102,8 +78,10 @@ def forward_message(message):
 					}
 			}
 		)
+		logger.info(f"Create dict in json about user {message.from_user.first_name}")
 	with open('data/users.json', "w") as file:
 		json.dump(users, file)
+		logger.info("Updated json file")
 
 
 def send_answer_to_user():
@@ -140,8 +118,10 @@ def send_answer_to_user():
 					bot.send_message(users[user]["chat_id"],
 					                 f"Дякуємо, {user}. Запит прийнято. Вже поспішаємо щоб його вирішити.")
 				users[user].update({"send_answer": True})
+				logger.info(f"Send automatic answer to user {user} (@{users[user]['username']})")
 				with open('data/users.json', "w") as file:
 					json.dump(users, file)
+					logger.info("Update json file")
 		time.sleep(5)
 
 
@@ -150,6 +130,7 @@ if __name__ == '__main__':
 	if os.path.exists('data/users.json'):
 		with open('data/users.json', 'r') as file:
 			users = json.load(file)
+			logger.info("Open json file")
 	else:
 		users = {}
 	
